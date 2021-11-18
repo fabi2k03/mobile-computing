7c494b0d8102e16a93828e8b1b419d61a025c3d7
Fabian Kopf <20180025@students.htl-perg.ac.at>
2021-11-18 12:38:50 +0100

    initial Message

---
+++ Kopf_NumberGuess/Kopf_NumberGuess/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kopf_NumberGuess
+//
+//  Created by Fabian Kopf on 18.11.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
