80bfa88cc1a59b47cd0327beb54e38416b7272a6
Fabian Kopf <20180025@students.htl-perg.ac.at>
2022-01-20 08:27:45 +0100

    Final Numberguess project

---
+++ NumberGuesser2/NumberGuess/ToDoModel.swift
@@ -0,0 +1,16 @@
+//
+//  ToDoModel.swift
+//  NumberGuess
+//
+//  Created by Fabian Kopf on 16.12.21.
+//
+
+import Foundation
+
+class ToDo{
+    var id = 0
+    var title = ""
+}
+class ToDoModel{
+    var todos = [ToDo]()
+}
